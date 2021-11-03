65bfeea01fa5b5856b943952025c4d9f7752ae54
Ivan <ivansantanacalle@gmail.com>
2021-11-03 22:04:09 +0100

    copyright added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,8 @@ function Footer() {
             <Link to='/' className='social-logo'> IV </Link>
           </div>
 
-          <small class='website-rights'>IV © 2020</small>
+          <small class='website-rights'>Copyright © 2021 - Iván Santana Calle
+</small>
           
           <div class='social-icons'>
             <Link class='social-icon-link facebook' to='/' target='_blank' aria-label='Facebook'> <i class='fab fa-facebook-f' /> </Link>

f94c62992e217e9997a6574bf5b661ac9be1af93
Ivan <ivansantanacalle@gmail.com>
2021-12-24 00:53:58 +0100

    Changed lang of the Hero Section ,deleted sections

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,15 +10,9 @@ function Footer() {
           <div class='footer-logo'>
             <Link to='/' className='social-logo'> IV </Link>
           </div>
-          
-          <div class='politics'>
-            <small class='website-rights'> Copyright © 2021 - Iván Santana Calle </small>
-            <p><a className="conditions" href="../pdfs/reporte2-05-10.pdf" download="Reporte2Mayo2010"> Condiciiones De Uso </a></p>
-            <p><a className="conditions" href="../pdfs/reporte2-05-10.pdf" download="Reporte2Mayo2010"> Condiciones Generales De Contratación </a></p>
-            <p><a className="conditions" href="../pdfs/reporte2-05-10.pdf" download="Reporte2Mayo2010"> Política de Cookies </a></p>
-            <p><a className="conditions" href="../pdfs/reporte2-05-10.pdf" download="Reporte2Mayo2010"> Política De Privacidad Redes Sociales </a></p>
 
-          </div>
+          <small class='website-rights'>IV © 2020</small>
+          
           <div class='social-icons'>
             <Link class='social-icon-link facebook' to='/' target='_blank' aria-label='Facebook'> <i class='fab fa-facebook-f' /> </Link>
             <Link class='social-icon-link instagram' to='/' target='_blank' aria-label='Instagram'> <i class='fab fa-instagram' /> </Link>
@@ -33,6 +27,3 @@ function Footer() {
 }
 
 export default Footer;
-
-//             <a href="../pdfs/reporte2-05-10.pdf" download="Reporte2Mayo2010"> Descargar Archivo </a>
-// para descargar archivos pdf de las políticas
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,16 +7,16 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/assets/videos/video-1.mp4' autoPlay loop muted />
-      <h1>Welcome guest!</h1>
-      <p>What are you waiting for?</p>
+      <h1>¡Bienvenido invitado!</h1>
+      <p>¿A qué estas esperando?</p>
 
       <div className='hero-btns'>
         <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large' >
-          VISIT MY GITLAB
+          VISITA MI GITLAB
         </Button>
 
         <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large' onClick={console.log('hey')} >
-          COSITAS VARIAS
+          DESCARGA MI CV
         </Button>
         
       </div>

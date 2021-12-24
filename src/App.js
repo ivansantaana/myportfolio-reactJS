421064da8f5e803135a8b175e0bf8e695ccce43e
Ivan <ivansantanacalle@gmail.com>
2021-12-24 03:03:38 +0100

    sing-up page deleted, moved cards to portfolio

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './components/pages/Home';
 import Services from './components/pages/Services';
-import SignUp from './components/pages/SignUp';
 import Portfolio from './components/pages/Portfoliio';
 
 import './App.css';
@@ -17,7 +16,6 @@ function App() {
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/services' component={Services} />
-          <Route path='/sign-up' component={SignUp} />
           <Route path='/portfolio' component={Portfolio} />
         </Switch>
       </Router>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/assets/videos/video-1.mp4' autoPlay loop muted />
-      <h1>¡Bienvenido invitado!</h1>
+      <h1>¡Bienvenido internauta!</h1>
       <p>¿A qué estas esperando?</p>
 
       <div className='hero-btns'>

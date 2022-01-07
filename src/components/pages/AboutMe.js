428f7096a4d031dad023abb6e326db29a3c4035f
Ivan <ivansantanacalle@gmail.com>
2022-01-07 18:33:39 +0100

    About e banner animated

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -3,10 +3,10 @@ import './AboutMe.css';
 
 export default function AboutMe() {
   return (
-    <view>
-      <img className='avatar' src= {require("./AboutMeAssets/AboutMeBanner.png")}/>
-      <h1 className='about'>About me</h1>
-    </view>
+    <div>
+      <img className='background' src= {require("./AboutMeAssets/banner.gif")}/>
+    </div>
+
 
   );
 }

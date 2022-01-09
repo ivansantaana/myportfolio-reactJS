0a70fa6bec883e2060b56c376fd55b6a9667e88f
Ivan <ivansantanacalle@gmail.com>
2022-01-09 13:04:02 +0100

    Adding curriculum to the AboutMe screen

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -3,10 +3,23 @@ import './AboutMe.css';
 
 export default function AboutMe() {
   return (
-    <div>
-      <img className='background' src= {require("./AboutMeAssets/banner.gif")}/>
+    <div className='AboutMeScreen'>
+        <img className='background' src= {require("./AboutMeAssets/banner.gif")}/>
+
+      <div className='resume'>
+        
+        <div className='avatar_box'>
+          <text className='name_surname'>IVÁN SANTANA</text> <br></br>
+          <text className='job'>Front-end developer</text>
+        </div>
+        <figure className='resume_image'>
+          <img className='avatar' src={require("./AboutMeAssets/avatar.jpg")} alt=""/>
+        </figure>
+
+      <div className='resume_information'>
+
+      </div>
+      </div>
     </div>
-
-
   );
 }

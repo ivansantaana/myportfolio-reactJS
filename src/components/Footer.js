80269da77e8436778640742a38ca1c24701e8b28
Ivan <ivansantanacalle@gmail.com>
2022-01-18 20:04:21 +0100

    Footer: social-area updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,6 +18,8 @@ function Footer() {
             <a class="social-icon-link instagram"href="https://www.instagram.com/ivansantaana_/" target="_blank"><i class='fab fa-instagram' /></a>
             <a class="social-icon-link youtube"href="https://www.youtube.com/channel/UC3FUJix8bb3Bf0y7Cd0EopA" target="_blank"><i class='fab fa-youtube' /></a>
             <a class="social-icon-link linkedin"href="https://www.linkedin.com/in/ivan-santana-calle/" target="_blank"><i class='fab fa-linkedin' /></a>
+            <a class="social-icon-link git"href="https://gitlab.com/ivansantaana" target="_blank"><i class='fab fa-gitlab' /></a>
+
           </div>
         </div>
       </section>
